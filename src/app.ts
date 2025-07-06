d69e58d40cab9c781378060c8d1697c2d6ffafdc
= <sohelf131@gmail.com>
2025-07-06 22:08:43 +0600

    add: origins of cors

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,20 @@ import { booksRoutes } from './app/controllers/book.controller'
 import { borrowRoutes } from './app/controllers/borrow.controller'
 const app: Application = express()
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://group-study-b6d75.web.app',
+    'https://group-study-b6d75.firebaseapp.com'
+  ],
+  credentials: true
+}));
 app.use(express.json())
 app.use('/api/books', booksRoutes)
 app.use('/api/borrow', borrowRoutes)
 
 app.get('/', (req: Request, res: Response) => {
-    res.send("Welcome to our library")
+    res.send("Welcome to our library Api")
 })
 
 export default app
