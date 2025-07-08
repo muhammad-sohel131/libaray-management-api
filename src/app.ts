b8bacb4228a42ef9ac6412301269fa04b8ddceb3
= <sohelf131@gmail.com>
2025-07-08 22:12:34 +0600

    add: Frontend link in cors

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,26 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors';
-import { booksRoutes } from './app/controllers/book.controller'
-import { borrowRoutes } from './app/controllers/borrow.controller'
-const app: Application = express()
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import { booksRoutes } from "./app/controllers/book.controller";
+import { borrowRoutes } from "./app/controllers/borrow.controller";
+const app: Application = express();
 
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://group-study-b6d75.web.app',
-    'https://group-study-b6d75.firebaseapp.com'
-  ],
-  credentials: true
-}));
-app.use(express.json())
-app.use('/api/books', booksRoutes)
-app.use('/api/borrow', borrowRoutes)
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://library-management-system-nine-blue.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use("/api/books", booksRoutes);
+app.use("/api/borrow", borrowRoutes);
 
-app.get('/', (req: Request, res: Response) => {
-    res.json({
-        message: 'Welcome to Library Management API'
-    })
-})
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    message: "Welcome to Library Management API",
+  });
+});
 
-export default app
-
+export default app;
