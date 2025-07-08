0c4b9f68583b8972e874c7bd8917fa89e06b3f54
= <sohelf131@gmail.com>
2025-07-08 22:12:34 +0600

    add: Frontend link in cors

--- dist/app.js
+++ dist/app.js
@@ -10,17 +10,17 @@ const borrow_controller_1 = require("./app/controllers/borrow.controller");
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
     origin: [
-        'http://localhost:5173',
-        'https://library-management-system-nine-blue.vercel.app'
+        "http://localhost:5173",
+        "https://library-management-system-nine-blue.vercel.app",
     ],
-    credentials: true
+    credentials: true,
 }));
 app.use(express_1.default.json());
-app.use('/api/books', book_controller_1.booksRoutes);
-app.use('/api/borrow', borrow_controller_1.borrowRoutes);
-app.get('/', (req, res) => {
+app.use("/api/books", book_controller_1.booksRoutes);
+app.use("/api/borrow", borrow_controller_1.borrowRoutes);
+app.get("/", (req, res) => {
     res.json({
-        message: 'Welcome to Library Management API'
+        message: "Welcome to Library Management API",
     });
 });
 exports.default = app;
